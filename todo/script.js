23ceae557603b2db3e91fed304178811f2f51c10
Ratnesh Kumar Singh <work.ratnesh@gmail.com>
2024-01-23 21:52:45 +0530

    Update script.js

--- todo/script.js
+++ todo/script.js
@@ -1,27 +1,54 @@
+var todoList = [];
+document.onload = function () {
+  var data = localStorage.getItem("todo_list");
+  if (data) {
+    data = JSON.parse(data);
+    for (var j = 0; j < data.length; j++) {
+      var todo = data[j];
+      addTodo(todo.title, todo.description, todo.id);
+    }
+  }
+};
 
-
-function Todo(title, description) {
-    this.title = title;
-    this.description = description;
+function Todo(title, description, id) {
+  this.title = title;
+  this.description = description;
+  this.id = id;
 }
 
-function addTodo() {
-    var title = document.getElementById('title').value;
-    var desc = document.getElementById('description').value;
-    var todo = new Todo(title, desc);
-    var ul = document.getElementById('todo_list');
-    var li = document.createElement('li');
-    li.innerText = todo.title + ' : ' + todo.description;
-    var btn = document.createElement('button');
-    btn.innerText = 'remove';
-    btn.onclick = removeTodo;
-    li.appendChild(btn);
-    ul.appendChild(li);
+function add() {
+  var title = document.getElementById("title").value;
+  var desc = document.getElementById("description").value;
+  var id = "id" + new Date().getTime();
+  addTodo(title, desc, id);
+}
+
+function addTodo(title, desc, id) {
+  var todo = new Todo(title, desc, id);
+  todoList.push(todo);
+  var ul = document.getElementById("todo_list");
+  var li = document.createElement("li");
+  li.setAttribute("index", id);
+  li.innerText = todo.title + " : " + todo.description;
+  var btn = document.createElement("button");
+  btn.innerText = "remove";
+  btn.onclick = removeTodo;
+  li.appendChild(btn);
+  ul.appendChild(li);
+  saveToMemory();
 }
 
 function removeTodo(e) {
-    var btn = e.target;
-    var li = btn.parentNode;
-    var ul = document.getElementById('todo_list');
-    ul.removeChild(li);
+  var btn = e.target;
+  var li = btn.parentNode;
+  var ul = document.getElementById("todo_list");
+  var index = li.getAttribute("index");
+  todoList.splice(index, 1);
+  ul.removeChild(li);
+  saveToMemory();
+}
+
+function saveToMemory() {
+  var str = JSON.stringify(todoList);
+  localStorage.setItem("todo_list", str);
 }
