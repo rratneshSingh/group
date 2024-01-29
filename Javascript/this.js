0cb139fecf53e6f4bd7a50231e2d2ac34cb8e390
Ratnesh Kumar Singh <work.ratnesh@gmail.com>
2024-01-29 21:14:27 +0530

    Update this.js

--- Javascript/this.js
+++ Javascript/this.js
@@ -1,10 +1,63 @@
-// this -> keyword
+// this - keyword
 
-console.log(this);
-
-function A() {
+function A(name, age) { // parameter
     console.log(this);
+    console.log(name, age);
 }
 
-A();
+// A('John', 20); // arguments
+
+// var newFn = A.bind({
+//     name: ''
+// });
+
+// newFn();
+
+A.call({
+    name: ''
+}, 'Rock', 29);
+
+A.apply({
+    name: ''
+}, ['Rock', 39]);
+
+class Student {
+
+    firstName = ''
+    constructor() {
+    }
+}
+
+class Employee {
+
+    firstName = ''
+    constructor() {
+    }
+
+    captalizeFirstName() {
+        // this -> undefined
+        this.firstName = this.firstName.toUpperCase();
+    }
+}
+
+// attibutes, methods
+
+var obj = new Employee();
+obj.firstName = 'John';
+obj.captalizeFirstName();
+
+var fn = obj.captalizeFirstName;
+fn.call(obj);
+console.log(obj);
+
+var student = new Student();
+student.firstName = 'Sam';
+fn.call(student);
+console.log(student);
+
+
+
+
+
+
 
